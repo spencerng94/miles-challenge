4f5cc6984d3e94b1e7ce254fb1756add8e1f8250
spencerng94 <spencerng94@gmail.com>
2020-10-06 20:43:48 -0700

    Commit

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -32,9 +32,9 @@ const middleware = [
   ...(window.__REDUX_DEVTOOLS_EXTENSION__ ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
 ]
 
-const enhancer = compose(
+const enhancer = composeWithDevTools(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const store = createStore(
