60f7a39510c73667b985b8264e4e7e3bef11659e
spencerng94 <spencerng94@gmail.com>
2020-10-06 21:15:26 -0700

    Updated store config

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -11,24 +11,22 @@ const middleware = [thunk];
 
 // const persistedState = loadState();
 
-const composeEnhancers = composeWithDevTools({
-  // options like actionSanitizer, stateSanitizer
-});
-
-const enhancer = compose(
-  applyMiddleware(...middleware),
-  typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
-      ? a => a
-      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const enhancer = compose(
+//   applyMiddleware(...middleware),
+//   typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+//       ? a => a
+//       : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//           window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
 const store = createStore(
-    rootReducer,
-    // persistedState,
-    initialState,
-    enhancer
-    // enhancer
+  rootReducer,
+  // persistedState,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 store.subscribe(throttle(() => {
